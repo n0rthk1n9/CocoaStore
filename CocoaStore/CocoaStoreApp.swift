65b241c056a3aca2eb18c1d47eed4db51750d116
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-23 21:08:14 +0200

    Initial Commit

---
+++ CocoaStore/CocoaStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocoaStoreApp.swift
+//  CocoaStore
+//
+//  Created by Jan Armbrust on 23.04.24.
+//
+
+import SwiftUI
+
+@main
+struct CocoaStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
