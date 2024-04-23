d5724b3fb88fbb4ddf0f2506c05904c672ce2aa5
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-23 21:10:33 +0200

    renaming

--- CocoaStore/MainView.swift
+++ CocoaStore/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  CocoaStore
 //
 //  Created by Jan Armbrust on 23.04.24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
