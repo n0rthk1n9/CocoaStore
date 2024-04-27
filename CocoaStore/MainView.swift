d5471580d90b3f8971f40ae85cebf89dc0272a0b
Jan Armbrust <jan.armbrust@googlemail.com>
2024-04-27 15:01:23 +0200

    display subscriptions in rudimentary view

--- CocoaStore/MainView.swift
+++ CocoaStore/MainView.swift
@@ -5,20 +5,45 @@
 //  Created by Jan Armbrust on 23.04.24.
 //
 
+import StoreKit
 import SwiftUI
 
 struct MainView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+  @State private var subscriptions: [Product]?
+
+  var sortedSubscriptions: [Product] {
+    subscriptions?.sorted { $0.price < $1.price } ?? []
+  }
+
+  var body: some View {
+    List {
+      ForEach(sortedSubscriptions) { subscription in
+        HStack(alignment: .top) {
+          Text("🎫")
+          VStack(alignment: .leading) {
+            HStack {
+              Text(subscription.displayName)
+                .font(.title3)
+                .fontWeight(.bold)
+              Spacer()
+              Text(subscription.displayPrice)
+            }
+            Text(subscription.description)
+              .font(.caption)
+          }
         }
-        .padding()
+      }
     }
+    .task {
+      subscriptions = try? await Product.products(for: [
+        "dev.xbow.CocoaStore.ticketsGold",
+        "dev.xbow.CocoaStore.ticketsSilver",
+        "dev.xbow.CocoaStore.ticketsBronze"
+      ])
+    }
+  }
 }
 
 #Preview {
-    MainView()
+  MainView()
 }
